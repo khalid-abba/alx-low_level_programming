62c29dc408d93e54c09a0c1e4f31cda925e3cc82
khalid-abba <khalidoabba1996@gmail.com>
2023-03-16 16:43:11 -0700

    fixes

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: Main function returns a string
+ *     * Return: ALways 0 (success)
+ *      */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
