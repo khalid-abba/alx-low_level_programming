d335c62014f20b8779006f8454ac54873d67bcae
khalid-abba <khalidoabba1996@gmail.com>
2023-03-17 08:40:52 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Description: Main function returns a string
- *     * Return: ALways 0 (success)
- *      */
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
+ */
+
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
