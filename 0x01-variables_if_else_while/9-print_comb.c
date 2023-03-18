6bca9e0c2c212077946183d0d77d9aacfa212fa5
khalid-abba <khalidoabba1996@gmail.com>
2023-03-18 10:20:12 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: This program
+ * all possible combinations of single-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+
+		if (i != '9')
+		{
+			putchar (',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
