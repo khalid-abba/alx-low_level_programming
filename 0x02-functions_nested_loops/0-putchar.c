2a3c89356ff140cf087029043cd4fcd7f9abc8e1
khalid-abba <khalidoabba1996@gmail.com>
2023-03-21 15:30:13 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "_putchar.h"
+
+/**
+ * main - Prints _putchar
+ * @void: Empty parameter list for main.
+ *
+ * Descreption: Prints _putchar w/out stdio
+ *
+ * Return: 0 for success
+*/
+int main(void)
+{	
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
