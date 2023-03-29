52ff9015ad9c87f851cdcb11493f8b55ad621b8f
khalid-abba <khalidoabba1996@gmail.com>
2023-03-29 07:16:34 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,50 @@
 #include "main.h"
 
-/**
- * _memset - fills memory with a constant byte
- * @s: pointer to memory
- * @b: constant byte used to replace
- * @n: number of  bytes to replace
- * Description: fills memory s with byte b for n bytes
- * Return: pointer to memory
- **/
+int get_length(char *s);
 
-char *_memset(char *s, char b, unsigned int n)
+/**
+ * _strcat - Concatenates the string pointed to by @src,
+ * including the terminating null byte,
+ * to the end of the string pointed to by @dest.
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ * Return: A pointer to the destination string @dest.
+ */
+char *_strcat(char *dest, char *src)
 {
-	for (; n > 0; n--)
+	int initial_end;
+	int j;
+
+	initial_end = get_length(dest);
+	j = 0;
+	while (src[j] != '\0')
 	{
-		s[n - 1] = b;
+		dest[initial_end + j] = src[j];
+		j++;
+	}
+	dest[initial_end + j] = '\0';
+
+	return (dest);
+}
+
+/**
+ * get_length - returns the length of a string
+ * @s: String whose length we wish to determine
+ *
+ * Return: Length of s
+ */
+int get_length(char *s)
+{
+	int length;
+	int i;
+
+	length = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		++length;
+		i++;
 	}
 
-	return (s);
+	return (length);
 }
