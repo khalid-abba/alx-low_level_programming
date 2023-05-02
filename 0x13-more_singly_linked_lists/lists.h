2100310e7ab61c4b8ff5dd936ad998035dec7f1d
khalid-abba <khalidoabba1996@gmail.com>
2023-05-02 21:19:06 +0100

    listes + task 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,10 +1,15 @@
-#ifndef LISTS_H
-#define LISTS_H
-#include <stdio.h>
+#ifndef _LIST_H_
+#define _LIST_H_
+#include <stddef.h>
 #include <stdlib.h>
 
 /**
- * singly linked list
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for alx project
  */
 typedef struct listint_s
 {
@@ -21,11 +26,11 @@ void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *insert_nodeint_at_index(listint_t **, unsigned int, int);
+int delete_nodeint_at_index(listint_t **, unsigned int);
+
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-
 #endif
